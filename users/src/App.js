7c15b305d2acbf5aa1c617262d005d8bef5bfbde
Jensen Koch <jtkoch12@gmail.com>
2020-04-08 20:25:23 -0500

    added cra for stretch

---
+++ users/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import axios from 'axios';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Yo</h1>
+    </div>
+  );
+}
+
+export default App;
